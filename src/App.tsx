ace13be6c583e47287ae6fb653c3b88eb39b41eb
Alexandru Munteanu <alexandru.munt@gmail.com>
2023-12-28 22:03:00 +0200

    setup: add supabase database

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import { Heading, VStack } from "@chakra-ui/react";
 
+import { supabase } from "../lib/supabase";
+
 function App() {
+  useEffect(() => {
+    getLots();
+  }, []);
+
+  const getLots = async () => {
+    const { data } = await supabase.from("parking-lots").select("*");
+
+    console.log("the result ->", data);
+  };
+
   return (
     <VStack h="100vh" justifyContent="center">
       <Heading>Parking App - Frontend</Heading>
