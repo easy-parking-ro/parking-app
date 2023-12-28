694d6a740e733b9ad62b79d7f03ab7055bfd7179
Alexandru Munteanu <alexandru.munt@gmail.com>
2023-12-28 16:48:41 +0200

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Heading, VStack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <VStack h="100vh" justifyContent="center">
+      <Heading>Parking App - Frontend</Heading>
+    </VStack>
+  );
+}
+
+export default App;
